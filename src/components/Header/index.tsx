928c8ee0cb1365936df3cb3d5952547608aa30b8
tomasi-metaversal <thomas@metaversal.gg>
2025-03-28 01:51:05 +1100

    create header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <h1 className="flex items-center gap-3 text-2xl font-semibold">
+      <span className="text-yellow-400">⭐</span>
+      <span>My Portfolio</span>
+    </h1>
+  );
+};
+
+export default Header;
